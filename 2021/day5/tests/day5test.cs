fd9f68aae1037b5888e32909962a82567c5ffec4
emmadavids <davidsek88@gmail.com>
2023-11-05 19:38:07 +0000

    tests successfully set up, not passing but set up at least..

--- 2021/day5/tests/day5test.cs
+++ 2021/day5/tests/day5test.cs
@@ -1,19 +1,22 @@
-using System;
-using Xunit;
+using NUnit.Framework;
+using System.Collections.Generic;
+using static Program;
 
+[TestFixture]
 public class ProgramTests
 {
-    [Fact]
+    [Test]
     public void TestOverlap()
     {
         // Arrange
+        List<Vent> vents = new List<Vent>(); // Create a list of Vent objects
         Program p = new Program();
 
-        // Act
-        p.ProcessInput();
+        // Act 
+        Program.VentUtilities.ProcessInput();
 
         // Assert
-        int overlaps = Program.GetOverlaps();
-        Assert.Equal(5, overlaps);
+        int overlaps = Program.VentUtilities.GetOverlaps(vents);
+        Assert.AreEqual(5, overlaps);
     }
 }
