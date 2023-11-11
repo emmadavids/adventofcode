a644b70e4e20b4ab45f00828fdf99b5ea3b0f743
emmadavids <davidsek88@gmail.com>
2023-11-11 23:12:15 +0100

    of course day 6 part 2 doesnt work

--- 2021/day6/src/Program.cs
+++ 2021/day6/src/Program.cs
@@ -7,23 +7,20 @@ public class Program
 {
     public static List<int> ParseInput(string input)
     {
-        string[] numberStrings = input.Split(",");
-        List<int> numbers = new List<int>();
-        foreach (string number in numberStrings)
-        {
-            int num = Convert.ToInt32(number);
-            numbers.Add(num);
-        }
-        return numbers;
+        // string[] numberStrings = input.Split(",");
+        // List<int> numbers = new List<int>();
+        // foreach (string number in numberStrings)
+        // {
+        //     int num = Convert.ToInt32(number);
+        //     numbers.Add(num);
+        // }
+        // return numbers;
+        return input.Split(",").Select(int.Parse).ToList();
     }
     public static void LanternFishSpawnCalculator(int days, List<int>fishput)
     {
         while (days != 0)
         {   
-            foreach (int num in fishput)
-            {
-                Console.WriteLine(num);
-            }
             for (int i = 0; i < fishput.Count; i++)
             {    
                 if (fishput[i] == 0)
@@ -39,9 +36,10 @@ public class Program
     }
 static void Main()
 {
-    string testInput = "3,4,3,1,2";
-    List<int> fishies = ParseInput(testInput);
-    LanternFishSpawnCalculator(80, fishies);
+    // string testInput = "3,4,3,1,2";
+    string text = File.ReadAllText("input.txt");
+    List<int> fishies = ParseInput(text);
+    LanternFishSpawnCalculator(256, fishies);
 }
 
 }
