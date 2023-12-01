505248cf16cc18ae73644c214a3d051ccc37bf15
emmadavids <davidsek88@gmail.com>
2023-12-01 21:23:40 +0000

    day 1 of 2023 complete :)

--- 2021/day8/Program.cs
+++ 2021/day8/Program.cs
@@ -1,2 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq;
+
+public class Program
+{  
+    static Dictionary<string, int> signalDecoderUnsorted = new Dictionary<string, int>(){
+        {"acedgfb", 8},
+        {"cdfbe", 5}, 
+        {"gcdfa", 2},
+        {"fbcad", 3},
+        {"dab", 7},
+        {"cefabd", 9},
+        {"cdfgeb", 6},
+        {"eafb", 4},  
+        {"cagedb", 0},
+        {"ab", 1}
+    }; //sort these 
+    
+    public static string SortChars(string s)
+    {
+        char[] chars = s.ToCharArray();
+        Array.Sort(chars);
+        return new string(chars);
+    }
+
+    static Dictionary<string, int> signalDecoder = new Dictionary<string, int>();
+
+    public static void SortDictionary()
+    {
+    foreach (KeyValuePair<string, int> pair in signalDecoderUnsorted)
+    {
+        string sortedKey = SortChars(pair.Key);
+        Console.WriteLine($"{sortedKey}, {pair.Value}");
+        signalDecoder.Add(sortedKey, pair.Value);
+    }
+    }
+
+    public static List<string> ParseInput(string text)
+    {
+        string[] lines = File.ReadAllLines(text);
+        List<string> afterDelimiterSegments = new List<string>();
+        foreach (string line in lines)
+        {   
+            string bisectedBit = line.Substring(line.IndexOf("|") + 1);
+            afterDelimiterSegments.Add(bisectedBit);
+        };
+        return afterDelimiterSegments;
+    }
+
+    public static void DetectChars(List<string> parsedInput)
+    {
+        List<int> allDecodedNums = new List<int>();
+        foreach (string line in parsedInput)
+        {   
+            string sortedKey;
+            // Console.WriteLine(line);
+            var decodedNums = line.Split(" ") 
+                                .Where(s => !String.IsNullOrEmpty(s))
+                                .Select(s => {
+                sortedKey = SortChars(s);
+                // Console.WriteLine(sortedKey);
+                int value;
+                if (signalDecoder.TryGetValue(sortedKey, out value))
+                { 
+                    Console.WriteLine($"value; {value}");
+                    return value;
+                }
+                else 
+                { 
+                    Console.WriteLine("fired");
+                    return 0;  
+                }       
+                });
+            allDecodedNums.AddRange(decodedNums);
+        }
+        List<int> decodedList = allDecodedNums.ToList();
+        foreach (int num in decodedList) {
+        Console.WriteLine(num);  
+        
+}
+    }
+        // var summy = allDecodedNums.SelectMany(x => Convert.ToInt32(x)).Sum();
+        // Console.WriteLine(summy);
+    
+    static void Main()
+    {
+        string filepath = "test_input.txt";
+        List<string> parsedInput = ParseInput(filepath);
+        SortDictionary();
+        DetectChars(parsedInput);
+    }
+}
---
+++ 2023/day1/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+List<string> lines = File.ReadLines("full_input.txt").ToList();
+List<int> numbers = new List<int>();
+
+Dictionary<string, string> wordNums = new Dictionary<string, string>
+            {
+            {"one", "1"},
+            {"two", "2"},
+            {"three","3"}, 
+            {"four", "4"},
+            {"five", "5"},
+            {"six", "6"},
+            {"seven", "7"},
+            {"eight", "8"},
+            {"nine", "9" }};
+
+foreach(string line in lines)
+{   
+    string num = "";
+    for (int i = 0; i < line.Length; i++)
+    {   
+        foreach (var (key,value) in wordNums)
+        {   
+            try{  
+            if (line.Substring(i, key.Length) == key)
+            {
+
+                num += value;
+            }}
+            catch {
+                continue;
+            }
+        }
+        if (Char.IsDigit(line[i]))
+        {
+            num += line[i];
+        }
+    }
+    if (num.Length > 2)
+    {   
+        num = (char)num[0] + num.Substring(num.Length - 1);
+    }
+    else if (num.Length == 1)
+    {
+        num = num + num;
+    }
+    try{
+    var intNum = Int32.Parse(num);
+    numbers.Add(intNum);
+    // Console.WriteLine(num);
+    }
+    catch { continue ;}
+}
+
+Console.WriteLine(numbers.Sum());
