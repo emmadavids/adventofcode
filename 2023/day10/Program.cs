629e0e2a59444c2fc6f7c375143948aeb1374383
emmadavids <davidsek88@gmail.com>
2023-12-26 10:22:07 +0000

    finall solved part 2 after asking claude ai to explain ray casting 500 times

--- 2023/day10/Program.cs
+++ 2023/day10/Program.cs
@@ -1,8 +1,10 @@
-﻿using System.Linq;
+﻿using System.Drawing;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Net;
 using System.Net.WebSockets;
 using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
 
 public class Program 
 {
@@ -12,8 +14,6 @@ public class Program
     public static List<char> possibleRightPipes = new() { '-', 'J', '7'};
     public static List<(int, int)> visitedNodes = new();
     public static int steps = 0;
-    public static List<Coordinate> coordinates = new();
-    
     public static int y = 0;
     public static int x = 0;
     public static char neighbouringPipe = 'S';
@@ -30,17 +30,6 @@ public class Program
 
     public static List<(int, int)> startNeighbouringCoords = new() { (0, -1), (0, 1), (-1, 0), (1, 0) };
     
-    public class Coordinate
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public Coordinate(int x, int y)
-        {
-            X = x;
-            Y = y;
-        } 
-    }
-    
     public static void FindStart(string[] lines)
     {   
         for (int i = 0; i < lines.Count(); i++)
@@ -49,11 +38,7 @@ public class Program
             {
                 x = i;
                 y = lines[i].IndexOf('S');
-                Console.WriteLine($"x:{x},y:{y}");
-                Coordinate startingCoordinate = new Coordinate(x, y);
-                coordinates.Add(startingCoordinate);
                 visitedNodes.Add((x, y));
-                 
             }   
         }
     }
@@ -66,14 +51,11 @@ public class Program
             int xCopy = x + startNeighbouringCoords[i].Item1;
             int yCopy = y + startNeighbouringCoords[i].Item2;
             neighbouringPipe = lines[xCopy][yCopy];
-            Console.WriteLine($"neighbouring pipe{neighbouringPipe}, neighbouring coordinates{startNeighbouringCoords[i]}");
             Tuple<int, int> neighbouringCoords = (xCopy, yCopy).ToTuple(); 
-            Console.WriteLine($"x: {xCopy} y: {yCopy}");
             if (possibleTopPipes.Contains(neighbouringPipe) && startNeighbouringCoords[i] == (-1, 0))
                 {
                     visitedNodes.Add((neighbouringCoords.Item1, neighbouringCoords.Item2));
                     neighbouringPipe = lines[xCopy][yCopy];
-                    Console.WriteLine($"returning neighbouring pipe:{neighbouringPipe}");
                     break;
                 }
             else if (possibleBottomPipes.Contains(neighbouringPipe) && startNeighbouringCoords[i] == (1, 0))
@@ -105,28 +87,16 @@ public class Program
             Console.WriteLine("fail");
         }
         }}
-        
-    
 
     public static void BuildPipeMap(string[] lines)
     {   
-        int count = 13700;
         bool keepLooping = true;
         
         while (keepLooping)
         {
-
-            // if (y -1 >= 0 && lines[x][y-1] == 'S')
-            // {   
-            //     visitedNodes.Add((x, y-1));
-            //     break;
-            // }
-        List<(int, int)> currentPipeDirection = exits[neighbouringPipe];
-        int x3 = x + currentPipeDirection[1].Item1;
-        int y3 = y + currentPipeDirection[1].Item2;
-        int x4 = x + currentPipeDirection[0].Item1;
-        int y4 = y + currentPipeDirection[0].Item2;
-
+            List<(int, int)> currentPipeDirection = exits[neighbouringPipe];
+            int x3 = x + currentPipeDirection[1].Item1;
+            int y3 = y + currentPipeDirection[1].Item2;
     
             currentPipeDirection = exits[neighbouringPipe];
             if (visitedNodes.Contains((x3,y3)))
@@ -140,11 +110,6 @@ public class Program
             }
             
             visitedNodes.Add((x, y));
-            Console.WriteLine($"char {neighbouringPipe} X: {x +1}, Y: {y}");
-                // foreach (var (coordX, coordY) in visitedNodes)
-                //     {
-                //         Console.WriteLine($"char {neighbouringPipe} X: {coordX}, Y: {coordY}");
-                //     }
 
             neighbouringPipe = lines[x][y];
             
@@ -153,18 +118,15 @@ public class Program
                 visitedNodes.Add((x, y-1));
                 break;
             }
-
-            count -=1;
     }}
-
 
     public static int CountSteps()
     {   
-        foreach (var (coordX, coordY) in visitedNodes)
-            {
-                Console.WriteLine($"char {neighbouringPipe} X: {coordX}, Y: {coordY}");
-            }
-        Console.WriteLine(visitedNodes.Count);
+        // foreach (var (coordX, coordY) in visitedNodes)
+        //     {
+        //         Console.WriteLine($"({coordX}, {coordY})");
+        //     }
+ 
         if (visitedNodes.Count % 2 == 0)
         {
             return visitedNodes.Count / 2;
@@ -173,8 +135,43 @@ public class Program
         {
             return (visitedNodes.Count - 1) / 2;
         }
-    }
+}
 
+public static bool IsBetween(int x, int y, List<(int, int)> pipeEdges)
+{
+    int overlaps = 0;
+
+    for (int i = 0, j = pipeEdges.Count - 1; i < pipeEdges.Count; j = i++)
+    {
+        if (((pipeEdges[i].Item2 <= y && y < pipeEdges[j].Item2) ||
+            (pipeEdges[j].Item2 <= y && y < pipeEdges[i].Item2)) &&
+            (x < (pipeEdges[j].Item1 - pipeEdges[i].Item1) *
+                    (y - pipeEdges[i].Item2) /
+                    (pipeEdges[j].Item2 - pipeEdges[i].Item2) +
+                pipeEdges[i].Item1))
+        {
+            overlaps++;
+        }
+    }
+    return overlaps % 2 != 0;
+}
+
+public static void CheckHowManyTilesAreInsideLoop(string[] lines)
+{
+    int tiles = 0;
+    for (int i = 0; i < lines.Count(); i++)
+    {
+        for (int j = 0; j < lines[i].Length; j++)
+        {   
+            if (!visitedNodes.Contains((i,j))){
+            if (IsBetween(i, j, visitedNodes))
+            {
+                tiles += 1;
+            }}
+        }
+    }
+    Console.WriteLine(tiles);
+}
 public static void Main()
 {   
     string[] lines = File.ReadAllLines("full_input.txt");
@@ -182,9 +179,6 @@ public static void Main()
     TraverseFromStart(lines);
     BuildPipeMap(lines);
     Console.WriteLine(CountSteps());
-
+    CheckHowManyTilesAreInsideLoop(lines);
 }
 }    
-
-
-
